bea84aac50a079a106c9dd17db386f211a2b160a
Alejandro Cabezas <alexcabezas0705@gmail.com>
2025-12-21 20:17:26 +0100

    Add io for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"path/filepath"
+)
+
 type Config struct {
 	OnlySuggest  bool   `json:"only_suggest"`
 	DefaultModel string `json:"default_model"`
@@ -12,17 +19,70 @@ type Provider interface {
 
 type DefaultProvider struct{}
 
+func NewProvider() Provider {
+	return DefaultProvider{}
+}
+
 func (provider DefaultProvider) Get() (error, Config) {
-	return nil, Config{
-		OnlySuggest:  false,
-		DefaultModel: "",
+	configPath, err := configPath()
+	if err != nil {
+		return err, Config{}
 	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return err, Config{}
+	}
+
+	defer closeFile(file)
+
+	byteValue, err := io.ReadAll(file)
+
+	if err != nil {
+		return err, Config{}
+	}
+
+	var config Config
+	err = json.Unmarshal(byteValue, &config)
+
+	if err != nil {
+		return err, Config{}
+	}
+
+	return nil, config
 }
 
 func (provider DefaultProvider) Set(cfg Config) error {
+	configPath, err := configPath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(configPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	defer closeFile(file)
+
+	byteValue, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(byteValue)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func NewProvider() Provider {
-	return DefaultProvider{}
+func closeFile(f io.Closer) {
+	if err := f.Close(); err != nil {
+	}
+}
+
+func configPath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(homeDir, ".whai", "config.json"), nil
 }
